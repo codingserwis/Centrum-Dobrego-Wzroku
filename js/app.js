762a7897ddd5f343322c0ca4d0dd49f1c150f2df
Maciej Lehrer <maciej.lehrer@gmail.com>
2017-06-19 17:00:25 +0200

    Added js to assets
    1. added js to assets
    2. minified js file
    3. deleted sticky-kit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Sticky menu
 function stickyMenu(element, element2){
 	$(window).scroll(function(event) {
 		if($(this).scrollTop() > element){
@@ -13,7 +14,7 @@ $(function() {
 		nav = $('nav'),
 		toTop = $('a[href="#top"]'),
 		elements = $('html, body');
-		
+// Scrolling to top
 		toTop.click(function(e) {
 			e.preventDefault();
 			elements.animate({scrollTop : 0}, 700);
@@ -22,6 +23,7 @@ $(function() {
 	stickyMenu(header, nav);
 
 });
+// Showing to top button on the right side of page
 $(document).scroll(function(){
 	var toTopBtn = $('.to-top-box'),
 		top = $(window).scrollTop();
