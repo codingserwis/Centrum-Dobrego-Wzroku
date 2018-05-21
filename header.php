661958177313e326f044b349bd9cb5225427a3e1
CodingSerwis <maciej.lehrer@codingserwis.pl>
2018-05-22 00:42:05 +0200

    added GTM

--- header.php
+++ header.php
@@ -4,8 +4,15 @@
  */
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php bloginfo( 'language' ); ?>>
 	<head>
+		<!-- Google Tag Manager -->
+			<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-53SB2S7');</script> -->
+		<!-- End Google Tag Manager -->
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,19 +28,13 @@
 		<meta name="theme-color" content="#ffffff">
 		<script src="https://use.fontawesome.com/b1f95d1c43.js"></script>
 		<title><?php bloginfo('name'); ?></title>
-		<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-89931638-4', 'auto');
-  ga('send', 'pageview');
-
-</script>
 		<?php wp_head(); ?>
 	</head>
 	<body>
+	<!-- Google Tag Manager (noscript) -->
+		<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53SB2S7"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
+	<!-- End Google Tag Manager (noscript) -->
 		<header class="header">
 			<div class="header_container">
 				<div class="row">
